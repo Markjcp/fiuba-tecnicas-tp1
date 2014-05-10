b1d813f9b8e4c5878c211da09cd8069001bd433d
Marcos <marcos@marcos-laptop.(none)>
2014-05-10 15:27:17 -0300

    Commit inicial

---
+++ src/test/java/ar/fiuba/tecnicas/tp1/BaseProjectTest.java
@@ -0,0 +1,14 @@
+package ar.fiuba.tecnicas.tp1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BaseProjectTest {
+	
+	@Test
+	public void testBaseConfiguration(){
+		assertEquals(true,true);
+	}
+
+}

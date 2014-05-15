cde21a4f65d553f0ed3b861991960d3665d1498c
Belu Beltran <belubeltran@gmail.com>
2014-05-15 14:46:52 -0300

    Clases de formato comentadas

--- src/main/java/ar/fiuba/tecnicas/tp1/logger/MessageFormat.java
+++ src/main/java/ar/fiuba/tecnicas/tp1/logger/MessageFormat.java
@@ -5,24 +5,47 @@ import java.util.Vector;
 
 import static ar.fiuba.tecnicas.tp1.factory.CreationConstants.*;
 
+/**
+ * Esta clase se encarga de parsear el formato a partir de un string.
+ * Puede definirse un separador para delimitar los diferentes formatos armados.
+ *
+ */
 public class MessageFormat {
 	
 	private Vector<String> format;
 	private String delimiter;
 	
+	/** Crea un MessageFormat a partir de un string y un delimitador.
+	 * Para el formato del dia se aceptan los mismos que para SimpleDateFormat.
+	 * Un ejemplo de formato podría ser: %d{HH:mm:ss} %p %t %m
+	 * @param format
+	 * @param delimiter
+	 */
 	public MessageFormat(String format, String delimiter) {
 		this.delimiter = delimiter;
 		this.format = new Vector<String>(Arrays.asList(format.split(delimiter)));
 	}
 	
+	/**
+	 * Método que obtiene la cantidad de formatos aplicados.
+	 * @return Cantidad de elementos de formato.
+	 */
 	public int getModifiersSize(){
 		return format.size();
 	}
 	
+	/**
+	 * Método que devuelve el separador definido en el archivo de propiedades.
+	 * @return Separador.
+	 */
 	public String getDelimiter() {
 		return this.delimiter;
 	}
 	
+	/**
+	 * Método que obtiene el formato de fecha seleccionado en el archivo de propiedades.
+	 * @return Formato de la fecha.
+	 */
 	public String getDateFormat() {
 		int resultFoundCode = dateIsVisible();
 		if(resultFoundCode!=NOT_FOUND_FORMAT_CODE){
@@ -32,6 +55,11 @@ public class MessageFormat {
 		return "";
 	}
 	
+	/**
+	 * Comprueba si en el formato se especificó la fecha (es decir, contiene un %d) 
+	 * para ser mostrada en el mensaje de log
+	 * @return Si encuentra o no el formato de fecha
+	 */
 	public int dateIsVisible() {
 		try {
 			int i = 0;
@@ -48,30 +76,65 @@ public class MessageFormat {
 		}
 	}
 	
+	/**
+	 * Comprueba si en el formato se especificó el nivel (es decir, contiene un %p) 
+	 * para ser mostrada en el mensaje de log
+	 * @return Si encuentra o no el formato de nivel
+	 */
 	public int levelIsVisible() {
 		return this.format.contains("%p") ? this.format.indexOf("%p") : NOT_FOUND_FORMAT_CODE;
 	}
 	
+	/**
+	 * Comprueba si en el formato se especificó el thread (es decir, contiene un %t) 
+	 * para ser mostrada en el mensaje de log
+	 * @return Si encuentra o no el formato de thread
+	 */
 	public int threadIsVisible() {
 		return this.format.contains("%t") ? this.format.indexOf("%t") : NOT_FOUND_FORMAT_CODE;
 	}
 	
+	/**
+	 * Comprueba si en el formato se especificó el mensaje (es decir, contiene un %m) 
+	 * para ser mostrada en el mensaje de log
+	 * @return Si encuentra o no el formato de mensaje
+	 */
 	public int messageIsVisible() {
 		return this.format.contains("%m") ? this.format.indexOf("%m") : NOT_FOUND_FORMAT_CODE;
 	}
 	
+	/**
+	 * Comprueba si en el formato se especificó el delimitador (es decir, contiene un %n) 
+	 * para ser mostrada en el mensaje de log
+	 * @return Si encuentra o no el formato de delimitador
+	 */
 	public int delimiterIsVisible() {
 		return this.format.contains("%n") ? this.format.indexOf("%n") : NOT_FOUND_FORMAT_CODE;
 	}
 	
+	/**
+	 * Comprueba si en el formato se especificó el numero de linea 
+	 * (es decir, contiene un %L) para ser mostrada en el mensaje de log
+	 * @return Si encuentra o no el formato de numero de linea
+	 */
 	public int lineNumberIsVisible() {
 		return this.format.contains("%L") ? this.format.indexOf("%L") : NOT_FOUND_FORMAT_CODE;
 	}
 	
+	/**
+	 * Comprueba si en el formato se especificó el nombre del archivo 
+	 * (es decir, contiene un %F) para ser mostrada en el mensaje de log
+	 * @return Si encuentra o no el formato de nombre de archivo
+	 */
 	public int fileNameIsVisible() {
 		return this.format.contains("%F") ? this.format.indexOf("%F") : NOT_FOUND_FORMAT_CODE;
 	}
 	
+	/**
+	 * Comprueba si en el formato se especificó el nombre del metodo 
+	 * (es decir, contiene un %M) para ser mostrada en el mensaje de log
+	 * @return Si encuentra o no el formato de nombre de metodo
+	 */
 	public int methodNameIsVisible() {
 		return this.format.contains("%M") ? this.format.indexOf("%M") : NOT_FOUND_FORMAT_CODE;
 	}
--- src/main/java/ar/fiuba/tecnicas/tp1/logger/MessageFormatApplier.java
+++ src/main/java/ar/fiuba/tecnicas/tp1/logger/MessageFormatApplier.java
@@ -6,22 +6,33 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Esta clase se encarga de aplicar el formato que obtiene de MessageFormat a un 
+ * mensaje de log en particular. 
+ *
+ */
 public class MessageFormatApplier {
-	
-	//TODO: Debe ser creado en el Factory
-	
+		
 	private MessageFormat messageFormat;
 	
+	/**
+	 * Constructor que a partir de un formato y un delimitador, devuelve un 
+	 * MessageFormatApplier
+	 * @param format
+	 * @param delimiter
+	 */
 	public MessageFormatApplier(String format, String delimiter) {
 		this.messageFormat = new MessageFormat(format, delimiter);
 	}
 	
-	@Deprecated
-	public String applyFormat(String message, Level level, String loggerName) {
-		return getDate() + getDelimiter() + getLevel(level) + getCurrentThread() + 
-				getMessage(message) + getLineNumber() + getFileName(loggerName) + getMethodName();
-	}
-	
+	/**
+	 * Método que devuelve el mensaje formateado según el formato especificado en 
+	 * el archivo de propiedades.
+	 * @param message
+	 * @param level
+	 * @param loggerName
+	 * @return mensaje formateado.
+	 */
 	public String buildMessage(String message, Level level, String loggerName){
 		int size = messageFormat.getModifiersSize();
 		String[] modifiers = new String[size];
@@ -66,6 +77,11 @@ public class MessageFormatApplier {
 		
 	}
 	
+	/**
+	 * Método que devuelve la fecha según el formato que obtuvo el MessageFormat.
+	 * Si no se especificó la fecha en el formato, entonces se devuelve un string vacío.
+	 * @return fecha formateada si corresponde.
+	 */
 	private String getDate() {
 		if (!this.messageFormat.getDateFormat().isEmpty()) {
 			DateFormat dateFormat = new SimpleDateFormat(this.messageFormat.getDateFormat());
@@ -74,33 +90,73 @@ public class MessageFormatApplier {
 		return "";
 	}
 	
+	/**
+	 * Método que devuelve el nivel según el formato que obtuvo el MessageFormat.
+	 * Si no se especificó el nivel en el formato, entonces se devuelve un string vacío.
+	 * @param level
+	 * @return nivel del mensaje si corresponde
+	 */
 	private String getLevel(Level level) {
 		return this.messageFormat.levelIsVisible()!=NOT_FOUND_FORMAT_CODE?level.toString():"";
 	}
 	
+	/**
+	 * Método que devuelve el thread según el formato que obtuvo el MessageFormat.
+	 * Si no se especificó el thread en el formato, entonces se devuelve un string vacío.
+	 * @return thread actual de donde se está loggeando si corresponde
+	 */
 	private String getCurrentThread() {
 		return this.messageFormat.threadIsVisible()!=NOT_FOUND_FORMAT_CODE ? 
 				Thread.currentThread().getName() : "";
 	}
 	
+	/**
+	 * Método que devuelve el número de línea según el formato que obtuvo el MessageFormat.
+	 * Si no se especificó el número de línea en el formato, entonces se devuelve un string vacío.
+	 * @return número de linea actual si corersponde
+	 */
 	private String getLineNumber() {
 		return this.messageFormat.lineNumberIsVisible()!=NOT_FOUND_FORMAT_CODE ? 
 				String.valueOf(Thread.currentThread().getStackTrace()[4].getLineNumber()) : "";
 	}
 	
+	/**
+	 * Método que devuelve el nombre del archivo según el formato que obtuvo el MessageFormat.
+	 * Si no se especificó el nombre del archivo en el formato, 
+	 * entonces se devuelve un string vacío.
+	 * @param fileName
+	 * @return nombre del archivo si corresponde
+	 */
 	private String getFileName(String fileName) {
 		return this.messageFormat.fileNameIsVisible()!=NOT_FOUND_FORMAT_CODE?fileName:"";
 	}
 	
+	/**
+	 * Método que devuelve el nombre del método según el formato que obtuvo el MessageFormat.
+	 * Si no se especificó el nombre del método en el formato, 
+	 * entonces se devuelve un string vacío.
+	 * @return nombre del método si corresponde
+	 */
 	private String getMethodName() {
 		return this.messageFormat.methodNameIsVisible()!=NOT_FOUND_FORMAT_CODE ? 
 				Thread.currentThread().getStackTrace()[4].getMethodName() : "";
 	}
 	
+	/**
+	 * Método que devuelve el mensaje según el formato que obtuvo el MessageFormat.
+	 * Si no se especificó el mensaje en el formato, entonces se devuelve un string vacío.
+	 * @param message
+	 * @return mensaje ingresado por el usuario si corresponde
+	 */
 	private String getMessage(String message) {
 		return this.messageFormat.messageIsVisible()!=NOT_FOUND_FORMAT_CODE ? message : "";
 	}
 	
+	/**
+	 * Método que devuelve el delimitador según el formato que obtuvo el MessageFormat.
+	 * Si no se especificó el delimitador en el formato, entonces se devuelve un string vacío.
+	 * @return delimitador definido en el archivo de propiedades
+	 */
 	private String getDelimiter() {
 		return this.messageFormat.getDelimiter();
 	}

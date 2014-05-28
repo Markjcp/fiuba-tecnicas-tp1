fbe36d9ccb9394f778ef0ea3b4eac1425c509dfc
Belu Beltran <belubeltran@gmail.com>
2014-05-27 21:21:20 -0300

    FormatTests: Más intuitivos.

--- src/test/java/ar/fiuba/tecnicas/tp1/FormatTests.java
+++ src/test/java/ar/fiuba/tecnicas/tp1/FormatTests.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import ar.fiuba.tecnicas.tp1.logger.MessageFormat;
-
 import static ar.fiuba.tecnicas.tp1.factory.CreationConstants.*;
 
 public class FormatTests {
@@ -13,61 +12,61 @@ public class FormatTests {
 	@Test
 	public void testLevelIsVisible() {
 		MessageFormat mf = new MessageFormat("%d{dd-MM-yy}-%p", "-");
-		assertEquals(mf.levelIsVisible() != NOT_FOUND_FORMAT_CODE, true);
+		assertNotEquals(mf.getlevelIndexInFormat(), NOT_FOUND_FORMAT_CODE);
 	}
 
 	@Test
 	public void testLevelIsNotVisible() {
 		MessageFormat mf = new MessageFormat("%d{dd-MM-yy}-%M", "-");
-		assertEquals(mf.levelIsVisible() != NOT_FOUND_FORMAT_CODE, false);
+		assertEquals(mf.getlevelIndexInFormat(), NOT_FOUND_FORMAT_CODE);
 	}
 
 	@Test
 	public void testThreadIsVisible() {
 		MessageFormat mf = new MessageFormat("%d{dd-MM-yy}-%p-%t-%L%", "-");
-		assertEquals(mf.threadIsVisible() != NOT_FOUND_FORMAT_CODE, true);
+		assertNotEquals(mf.getThreadIndexInFormat(), NOT_FOUND_FORMAT_CODE);
 	}
 
 	@Test
 	public void testThreadIsNotVisible() {
 		MessageFormat mf = new MessageFormat("%d{dd-MM-yy}-%p-%L", "-");
-		assertEquals(mf.threadIsVisible() != NOT_FOUND_FORMAT_CODE, false);
+		assertEquals(mf.getThreadIndexInFormat(), NOT_FOUND_FORMAT_CODE);
 	}
 
 	@Test
 	public void testLineNumberIsVisible() {
 		MessageFormat mf = new MessageFormat("%d{dd-MM-yy}-%p-%M-%L", "-");
-		assertEquals(mf.lineNumberIsVisible() != NOT_FOUND_FORMAT_CODE, true);
+		assertNotEquals(mf.getLineNumberIndexInFormat(), NOT_FOUND_FORMAT_CODE);
 	}
 
 	@Test
 	public void testLineNumberIsNotVisible() {
 		MessageFormat mf = new MessageFormat("%p", "-");
-		assertEquals(mf.lineNumberIsVisible() != NOT_FOUND_FORMAT_CODE, false);
+		assertEquals(mf.getLineNumberIndexInFormat(), NOT_FOUND_FORMAT_CODE);
 	}
 
 	@Test
 	public void testFileNameIsVisible() {
 		MessageFormat mf = new MessageFormat("%F-%d{dd-MM-yy}-%p-%M-%L", "-");
-		assertEquals(mf.fileNameIsVisible() != NOT_FOUND_FORMAT_CODE, true);
+		assertNotEquals(mf.getFileNameIndexInFormat(), NOT_FOUND_FORMAT_CODE);
 	}
 
 	@Test
 	public void testFileNameIsNotVisible() {
 		MessageFormat mf = new MessageFormat("%p-%M", "-");
-		assertEquals(mf.fileNameIsVisible() != NOT_FOUND_FORMAT_CODE, false);
+		assertEquals(mf.getFileNameIndexInFormat(), NOT_FOUND_FORMAT_CODE);
 	}
 
 	@Test
 	public void testMethodNameIsVisible() {
 		MessageFormat mf = new MessageFormat("%d{dd-MM-yy}-%p-%M-%L", "-");
-		assertEquals(mf.methodNameIsVisible() != NOT_FOUND_FORMAT_CODE, true);
+		assertNotEquals(mf.getMethodNameIndexInFormat(), NOT_FOUND_FORMAT_CODE);
 	}
 
 	@Test
 	public void testMethodNameIsNotVisible() {
 		MessageFormat mf = new MessageFormat("%p-%L", "-");
-		assertEquals(mf.methodNameIsVisible() != NOT_FOUND_FORMAT_CODE, false);
+		assertEquals(mf.getMethodNameIndexInFormat(), NOT_FOUND_FORMAT_CODE);
 	}
 
 }
